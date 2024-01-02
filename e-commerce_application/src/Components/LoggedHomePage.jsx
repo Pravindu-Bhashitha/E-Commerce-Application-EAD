3d5a827553cdad26ca60444d8e7b7273adfa5ff7
Pravindu <pravindubh@gmail.com>
2024-01-02 18:08:16 +0530

    Some Changes done

--- e-commerce_application/src/Components/LoggedHomePage.jsx
+++ e-commerce_application/src/Components/LoggedHomePage.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Good from './Good';
 import { items } from '../Items';
 import './HomePage.css';
 import Topbarwithcart from './Topbarwithcart';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const LoggedHomePage = (props) => {
-  console.log(props);
   const userName = props?.location?.state?.userName || 'Guest';
-  console.log(userName);
+
+  useEffect(() => {
+    toast.success(`Welcome, ${userName}!`, {
+      position: 'top-center',
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+    });
+  }, [userName]);
   return (
-    <div>   
-      <Topbarwithcart className="cart"/>
+    <div>
+      <Topbarwithcart className="cart" />
       <h1>Hi {userName}</h1>
       <div className='good_component'>
         {items.map(item => (
