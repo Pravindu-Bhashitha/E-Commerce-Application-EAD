01cbbe883ec73e277aef1aca0f615979e0fe8d63
Pravindu <pravindubh@gmail.com>
2024-01-08 10:02:30 +0530

    API created to get products

---
+++ e_commerce_application_backend/E_com_backend/Controllers/ProductContoller.cs
@@ -0,0 +1,28 @@
+﻿using E_com_backend.Data;
+using E_com_backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace E_com_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductContoller : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public ProductContoller(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        [HttpGet("AllProductDetails")]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _dataContext.Products.ToListAsync();
+
+        }
+    }
+}
